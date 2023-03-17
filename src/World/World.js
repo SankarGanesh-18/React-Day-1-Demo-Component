0ded94a9c99b56ef8b247cca39ec16b3ecb2dfe7
SankarGanesh-18 <122220555+SankarGanesh-18@users.noreply.github.com>
2023-03-17 16:26:59 +0530

    Add files via upload

---
+++ src/World/World.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class World extends Component {
+  render() {
+    return (
+      <div><p>This is World Functional Component</p></div>
+    )
+  }
+}
+
+export default World
